7fff137f8c1af611e30a5c33d5f8955027b387a3
Samuel Pappas <spappas@trinity.edu>
2024-12-14 23:14:16 -0600

    fuzzer in scala in process of creation

---
+++ fuzzer.scala
@@ -0,0 +1,56 @@
+import java.io.File 
+import java.io.PrintWriter 
+import java.io.{File, FileInputStream, FileOutputStream}
+import scala.util.Random
+import scala.util.Using
+
+object Fuzzer {
+    def create_seed_file(): Unit =  {// usually just done the first time you run the fuzzer, creates a seed file 
+        val defaultSeed = "InitialSeedData".getBytes("UTF-8")        
+        val seed_file = new File("_seed_" )    
+        val print_Writer = new PrintWriter(seed_file)  
+        // Writing to the file        
+        //print_Writer.write(defaultSeed)  
+        //print_Writer.close()      
+        //println(s"Created a default seed file with content: $defaultSeed ")
+        Using(new FileOutputStream(seed_file)) { fos =>
+            fos.write(defaultSeed)
+        }
+        //println(s"Created a default seed file with content: ${new String(defaultSeed)}", Console.err)
+        Console.err.println(s"Created a default seed file with content: ${new String(defaultSeed)}")
+    } 
+
+    def fuzzThisThing(prngSeed: Int, iterations: Int): Unit = {
+        val seed_file = new File("_seed_")
+        if (!seed_file.exists()) {
+            create_seed_file()
+        } 
+        val data = Using(new FileInputStream(seed_file)) { fis =>
+            val bytes = Array.fill[Byte](seed_file.length().toInt)(0)
+            fis.read(bytes)
+            bytes
+        }.getOrElse {
+            throw new RuntimeException("Failed to read seed file")
+        }
+        val random = new Random(prngSeed)
+        val byteArray = data.toBuffer
+
+        for (_ <- 1 to iterations){
+            for (i <- byteArray.indices){
+                if (random.nextDouble() < 0.13) { // 13% chance to mutate
+                     byteArray(i) = random.nextInt(256).toByte
+                }
+            }
+        }
+
+
+
+    }
+
+    def testMethod(): Unit = {
+        println("hey")
+    }
+    def main(args: Array[String]) = {
+        println("Hello, world")
+    }
+}
